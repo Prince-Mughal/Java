cba89721fa032ea8aa99a17d71605a5b460e1bc5
Prince Mughal <princeb92@hotmail.com>
2018-08-14 01:11:23 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+/* Test File */
+package company;
+
+public class Main {
+    public static void main(String[] args){
+        Database database = new Database("root","");
+        if(database.connectToMySqlServer()){
+            System.out.println("Connected to MySQL Server.");
+        }
+        database.setDatabaseName("LetsChat");
+        if(database.connectToDatabase()){
+            System.out.println("Connected to Database.");
+        }
+        database.setDatabaseTableName("Student");
+        if(database.createTable("STU_ID INT NOT NULL,"
+                              + "STU_NAME VARCHAR(48) NOT NULL,"
+                              + "PRIMARY KEY (STU_ID)")){
+            System.out.println("Table Created.");
+        }
+        if(database.insertInto("001,\"Prince Mughal\"")){
+            System.out.println("Record Inserted.");
+        }
+    
+    }
+    
+}
